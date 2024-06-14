0c9cc0263e4dc34029bfd5919d2f5cd0b2a4b995
pael <fedorov2777dmitry@gmail.com>
2024-06-14 16:56:11 +0500

    edited README.md

--- setup.py
+++ setup.py
@@ -2,10 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='segfault_package',
-    version='0.1',
-    description='If you ever missed segfaults in Python, there you go',
+    version='0.0.1',
     author='pael',
+    description='If you ever missed segfaults in Python, there you go',
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/fdmx2301/segfault.git",
     packages=find_packages(),
     package_data={'segfault': ['../libsegfault.so']},
     include_package_data=True,
+    python_requires='>=3.6',
 )
