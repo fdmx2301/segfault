d92deaa7fb81bf89f2d741925ee356ae60243c0b
pael <fedorov2777dmitry@gmail.com>
2024-06-14 17:07:00 +0500

    edited setup

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 from setuptools import setup, find_packages
 
+with open("README.md", encoding="utf-8") as f:
+    long_description = f.read()
+
 setup(
     name='segfault_package',
     version='0.0.1',
     author='pael',
     description='If you ever missed segfaults in Python, there you go',
-    long_description=open("README.md").read(),
+    long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/fdmx2301/segfault.git",
     packages=find_packages(),
